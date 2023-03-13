93618f42200a040d32296f2ab97cfec1a0f3e7a5
Stonehammer <jrwstone@gmail.com>
2023-03-13 13:36:14 -0400

    test

--- CodeScanTest/CodeScanTest/Function1.cs
+++ CodeScanTest/CodeScanTest/Function1.cs
@@ -2,7 +2,6 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-using Microsoft.Data.SqlClient;
 
 namespace CodeScanTest
 {
@@ -12,11 +11,7 @@ namespace CodeScanTest
         public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-
-            using (SqlCommand cm = new()){
-                cm.CommandType  = System.Data.CommandType.Text;
-                cm.CommandText = "SELECT * FROM tables where ID = " + "hello";
-            }
+           
         }
     }
 }
--- codescantest.js
+++ codescantest.js
@@ -1,2 +1,3 @@
 var j = 1;
 eval("console.log('is this safe?')");
+window.open("http:google.com");
